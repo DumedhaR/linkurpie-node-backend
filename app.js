0b2710f14d0c62bbab2747ceb5094dd058253644
DumedhaR <pererarukshan45@gmail.com>
2025-08-15 08:25:11 +0530

    update server configs

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import contentRoutes from "./routes/contentRoutes.js";
+import helmet from "helmet";
+import rateLimiter from "express-rate-limiter";
 
 const app = express();
 
 app.set("query parser", "extended");
 
-app.use(express.json());
+app.use(express.json({}));
+
+app.use(helmet());
+
+const limiter = rateLimiter({
+  max: 1000,
+  windowMin: 60 * 60 * 1000,
+  message: "too much request from this IP, Please try again later!",
+});
+app.use("/api", limiter);
 
 app.get("/", (req, res) => {
   res.send("Link your pie to the world 😃🥧");
